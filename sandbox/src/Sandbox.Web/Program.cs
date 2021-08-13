3b1ff3223d6522d52368787b887dd0ede88a5a95
enisn <info@enisnecipoglu.com>
2021-08-13 12:39:53 +0300

    Add logs to console

--- sandbox/src/Sandbox.Web/Program.cs
+++ sandbox/src/Sandbox.Web/Program.cs
@@ -20,10 +20,7 @@ namespace Sandbox.Web
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
                 .Enrich.FromLogContext()
-                .WriteTo.Async(c => c.File("Logs/logs.txt"))
-#if DEBUG
                 .WriteTo.Async(c => c.Console())
-#endif
                 .CreateLogger();
 
             try
