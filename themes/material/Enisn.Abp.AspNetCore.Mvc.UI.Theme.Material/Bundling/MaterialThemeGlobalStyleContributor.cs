ec5af2e64c5249d7464f2c87e0a8686d70611b0d
enisn <info@enisnecipoglu.com>
2021-10-13 17:48:37 +0300

    Update theming support

--- themes/material/Enisn.Abp.AspNetCore.Mvc.UI.Theme.Material/Bundling/MaterialThemeGlobalStyleContributor.cs
+++ themes/material/Enisn.Abp.AspNetCore.Mvc.UI.Theme.Material/Bundling/MaterialThemeGlobalStyleContributor.cs
@@ -8,7 +8,7 @@ namespace Enisn.Abp.AspNetCore.Mvc.UI.Theme.Material.Bundling
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
             context.Files.Add(CultureHelper.IsRtl ? "/themes/material/css/mdb.rtl.min.css" : "/themes/material/css/mdb.min.css");
-            context.Files.Add("/themes/material/css/admin.css");
+            context.Files.Add("/themes/material/css/theme.css");
         }
     }
 }
